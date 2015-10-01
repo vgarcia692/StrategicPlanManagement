5daefcecbe5d84cd01e8186e13e4376c66057f36
vgarcia692 <victor.lemae.garcia@gmail.com>
2015-10-01 15:28:42 +1200

    change kpi datatype

--- models/kpis.js
+++ models/kpis.js
@@ -3,11 +3,11 @@ module.exports = function(sequelize, DataTypes) {
   var KPIs = sequelize.define('KPIs', {
     keyPerformanceIndicator: DataTypes.TEXT,
     keyTarget: DataTypes.TEXT,
-    base: DataTypes.FLOAT,
-    current: DataTypes.FLOAT,
-    target2016: DataTypes.FLOAT,
-    target2017: DataTypes.FLOAT,
-    target2018: DataTypes.FLOAT   
+    base: DataTypes.STRING,
+    current: DataTypes.STRING,
+    target2016: DataTypes.STRING,
+    target2017: DataTypes.STRING,
+    target2018: DataTypes.STRING   
   }, {
     classMethods: {
       associate: function(models) {
